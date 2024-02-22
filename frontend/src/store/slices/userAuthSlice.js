474cbaa822f4fc51bdba1742d53700d0e23fa049
Heisenberg <omaradelawad20013@gmail.com>
2024-02-22 09:10:10 +0200

    created rtk store and auth slice and handle the user authentication

---
+++ frontend/src/store/slices/userAuthSlice.js
@@ -0,0 +1,85 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const token = localStorage.getItem("token");
+axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+const initialState = {
+  user: null,
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (registrationData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/users/register`,
+        registrationData
+      );
+      if (response.data.access_token) {
+        localStorage.setItem("token", response.data.access_token);
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${response.data.access_token}`;
+      }
+
+      return response.data.user;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue("An error occurred while registering.");
+      }
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/users/login`,
+        credentials
+      );
+      if (response.data.access_token) {
+        localStorage.setItem("token", response.data.access_token);
+        axios.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${response.data.access_token}`;
+      }
+      return response.data.user;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue("An error occurred while logging in.");
+      }
+    }
+  }
+);
+
+const userAuthSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      localStorage.removeItem("token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload;
+      });
+  },
+});
+
+export default userAuthSlice.reducer;
