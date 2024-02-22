d93df91e14d3a83016333dcd1fe8f1df5edb26e8
Heisenberg <omaradelawad20013@gmail.com>
2024-02-22 05:51:20 +0200

    created the todo form component

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,5 +1,51 @@
-const TodoForm = () => {
-  return <div>TodoForm</div>;
-};
+import { useState } from "react";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
 
+const TodoForm = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (title && description && dueDate) {
+      console.log(title, description, dueDate);
+      setTitle("");
+      setDescription("");
+      setDueDate("");
+    }
+  };
+
+  return (
+    <div className="lg:h-fit lg:sticky lg:top-[6.2rem] mb-4 lg:mb-0 pb-12 lg:pb-0">
+      <h2 className="text-2xl font-bold mb-4">Create Todo</h2>
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <Input
+          id="title"
+          name="title"
+          placeholder="Enter todo title"
+          value={title}
+          onChange={setTitle}
+        />
+        <Input
+          id="description"
+          name="description"
+          placeholder="Enter todo description"
+          value={description}
+          onChange={setDescription}
+        />
+        <Input
+          type="date"
+          id="dueDate"
+          name="dueDate"
+          value={dueDate}
+          onChange={setDueDate}
+        />
+
+        <Button type="submit" text="Create" />
+      </form>
+    </div>
+  );
+};
 export default TodoForm;
--- frontend/src/pages/TodosPage.jsx
+++ frontend/src/pages/TodosPage.jsx
@@ -10,10 +10,7 @@ const TodosPage = () => {
 
   return (
     <div className="mt-24 w-full px-8 grid grid-cols-1 lg:grid-cols-3 lg:gap-10">
-      <div className="h-fit sticky top-[6.2rem]">
-        <TodoForm />
-      </div>
-
+      <TodoForm />
       <div className="col-span-2">
         <div className="flex flex-col md:flex-row justify-between items-center gap-3 md:gap-0">
           <h1 className="flex flex-row items-center gap-2 text-3xl font-bold">
