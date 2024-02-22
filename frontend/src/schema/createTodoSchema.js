530dbb09471ebaa1cfe31b1028336195a1549354
Heisenberg <omaradelawad20013@gmail.com>
2024-02-22 12:14:12 +0200

    created zod schema for the todo creation function

---
+++ frontend/src/schema/createTodoSchema.js
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const createTodoSchema = z.object({
+  title: z.string().min(1).max(50),
+  description: z.string().min(1).max(500),
+  dueDate: z.date(),
+});
+
+export default createTodoSchema;
