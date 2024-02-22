76761f4b62bd4e9291c7b18bc5ca061faf52f890
Heisenberg <omaradelawad20013@gmail.com>
2024-02-22 16:17:31 +0200

    custom error messages in all the schemas

--- frontend/src/schema/createTodoSchema.js
+++ frontend/src/schema/createTodoSchema.js
@@ -1,9 +1,19 @@
 import { z } from "zod";
 
 const createTodoSchema = z.object({
-  title: z.string().min(1).max(50),
-  description: z.string().min(1).max(500),
-  dueDate: z.coerce.date(),
+  title: z
+    .string()
+    .min(1, { message: "Title should be at least 1 character" })
+    .max(50, { message: "Title should be at most 50 characters" }),
+
+  description: z
+    .string()
+    .min(1, { message: "Description should be at least 1 character" })
+    .max(500, { message: "Description should be at most 500 characters" }),
+
+  dueDate: z.coerce
+    .date()
+    .refine((value) => !isNaN(value), { message: "Invalid date format" }),
 });
 
 export default createTodoSchema;
--- frontend/src/schema/signupSchema.js
+++ frontend/src/schema/signupSchema.js
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 const signupSchema = z.object({
-  name: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(6),
+  name: z.string().min(3, "Name should be at least 3 characters"),
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z.string().min(6, "Password should be at least 6 characters"),
 });
 
 export default signupSchema;
