f08b89dfef00e4753cdf1d308cf4f1b9fe3f9f59
Heisenberg <omaradelawad20013@gmail.com>
2024-02-22 03:50:52 +0200

    created button and input ui components

---
+++ frontend/src/ui/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ type, text, disabled, onClick }) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type || "button"}
+      className="transition duration-500 font-medium w-full py-2 px-4 bg-sky-500 text-white tracking-wide hover:bg-sky-600 rounded"
+      onClick={onClick || (() => {})}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
